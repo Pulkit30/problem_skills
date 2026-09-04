8ce38774610f045ad27c3f15a9b76bcdef491b8a
pulkitSharma <pulkitsharma1816@gmail.com>
2026-09-04 22:21:49 +0530

    added loader,error message and no users found message

--- my-frontend/src/App.jsx
+++ my-frontend/src/App.jsx
@@ -2,25 +2,49 @@ import { useEffect, useState } from "react";
 
 export const useUsers = () => {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
   useEffect(() => {
     const getUsers = async () => {
       try {
+        setLoading(true);
+        setError("");
+        await new Promise((resolve) => setTimeout(resolve, 1000));
         const response = await fetch(
           "https://jsonplaceholder.typicode.com/users",
         );
+        if (!response.ok) {
+          throw new Error(`Request failed: ${response.status}`);
+        }
         const userData = await response.json();
         setUsers(userData);
       } catch (error) {
         console.log(error);
+        setError("Failed to fetch users");
+      } finally {
+        setLoading(false);
       }
     };
     getUsers();
   }, []);
-  return users;
+  return { users, loading, error };
 };
 const App = () => {
-  const users = useUsers();
+  const { users, loading, error } = useUsers();
   const [searchTerm, setSearchTerm] = useState("");
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
+
+  const noUser = filteredUsers.length === 0;
+
+  if (loading) {
+    return <div>Loading users...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
   return (
     <>
       <input
@@ -29,19 +53,19 @@ const App = () => {
         onChange={(e) => setSearchTerm(e.target.value)}
         placeholder="Search by User Name"
       />
-      <ul>
-        {users
-          .filter((user) =>
-            user.name.toLowerCase().includes(searchTerm.toLowerCase()),
-          )
-          .map((user) => {
+      {noUser ? (
+        <p>No users found.</p>
+      ) : (
+        <ul>
+          {filteredUsers.map((user) => {
             return (
               <li key={user.id}>
                 {user.name} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {user.email}
               </li>
             );
           })}
-      </ul>
+        </ul>
+      )}
     </>
   );
 };
